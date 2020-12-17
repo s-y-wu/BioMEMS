ab30d318135176dc942a3cbfd0c88d646dbf87e1
Sean Wu <sywu@hmc.edu>
2020-12-16 19:54:21 -0800

    formalize Flow.jl
    Document flow function and helper function.
    Include Sonali's summer report explanation.
    Funciton names and variables using Julia style, except:
    - wallY should be changed to wall_y for the big style change edit
    - constants are in all caps with underscores, like V_MAX

--- src/ArlettModel_December/Flow.jl
+++ src/ArlettModel_December/Flow.jl
@@ -3,19 +3,41 @@
 
 #include("ArlettParameters.jl")
 
-"Flow bias using laminar flow approximations. Flow bias increases with y"
-function flow(XY)
-    if FLOW_OFF
-        return 0
-    end
+"   flow
 
+Analyze hydrogen peroxide position to compute the magnitude of flow
+
+By laminar flow, all nonzero flow biases the particle downstream
+in the positive x direction.
+"
+function flow(XY)
     y = XY[2]
-    if y < wallY || inEnz(XY)
+    if FLOW_OFF || y < wallY || inEnz(XY)
         return 0
     else
-        speed = 6327*(1 - ((1270 - y)^2) / (1270^2))
-        secondsPerStep = 0.0000027077
-        xDisplacementBias = speed * secondsPerStep
+        xDisplacementBias = getSpeed(y) * SECONDS_PER_STEP
         return xDisplacementBias
     end
+end
+
+# lengths are in microns
+const V_MAX = 6326
+const R = 1270
+
+"   getSpeed
+
+Compute the approximate laminar flow bias on the particle; magnitude of flow increases with the y-coordinate
+
+Excerpt from \"Simulating Chemical Cross-Talk in High Density Enzymatic Biosensors\" by Sonali Madisetti:
+\" In the experimental conditions, the probe had a constant flow of water, in which water entered the
+system at 40 mL/s for 700 seconds through a 0.21 in x 0.1 in pipe and exited at a similar rate. The radius
+of the rectangular probe apparatus was around 1270 microns. From Q = v_eff * A, where Q represented the
+flow-rate, v_eff was approximately 4217 um/s. For a square pipe, it can be derived that v_eff = (2/3)v_max,
+and thus that v_max = 6326 um/s. From there, using the velocity equation
+                                    v(r) = v_max * (1 - r^2 / R^2)
+where r is the y-distance from the center and R is the radius of the pipe. \"
+"
+function getSpeed(y)
+    r = R - y
+    return V_MAX * (1 - (r^2) / (R^2))
 end

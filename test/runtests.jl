0022f43cb1ef2d2b85940f937492b0f48421a7d6
Sean Wu <sywu@hmc.edu>
2020-12-28 16:09:55 -0800

    update runtests, use AWS Gravitron2

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,7 @@ using SafeTestsets
 
     @time @safetestset "Enz Backend Tests" begin include("enz_backend_test.jl") end
     @time @safetestset "Enz Frontend Tests" begin include("enz_frontend_test.jl") end
+
+    @time @safetestset "PPD Backend Tests" begin include("ppd_backend_test.jl") end
+    @time @safetestset "PPD Frontend Tests" begin include("ppd_frontend_test.jl") end 
 end
